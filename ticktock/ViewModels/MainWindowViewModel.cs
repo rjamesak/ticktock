bad88bf9aad80e5d3643166702edd4d800e915a0
Russell James <jamesru@oregonstate.edu>
2024-03-19 13:02:51 -0800

    Add project files.

---
+++ ticktock/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticktock.ViewModels
+{
+    class MainWindowViewModel
+    {
+        // Need properties for:
+        // ElapsedTime, ElapsedTimeText
+        // SecondHandX, SecondHandY
+        // ProgressBarValue
+
+        // Commands: StartStopCommand, ResetCommand
+
+    }
+}
